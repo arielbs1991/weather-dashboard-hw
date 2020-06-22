43b1cdbb6f392e186d4341d7f47709ff1fe25e32
Ariel <arielbs1991@gmail.com>
2020-06-21 21:56:08 -0700

    got my queryURL working

--- script.js
+++ script.js
@@ -15,16 +15,31 @@
 // THEN I am again presented with current and future conditions for that city
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
+console.log("hello");
+var citySearch = JSON.stringify($(".searchBtn").innerHTML);
 
-var citySearch = $(".searchBtn").innerHTML;
+//primary api key: 096ef74ec84603cb5d797eef6a3f0327
+//backup api key: 51eff38dc476b28387cdbdbd9705ea5b
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=096ef74ec84603cb5d797eef6a3f0327";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=51eff38dc476b28387cdbdbd9705ea5b&units=imperial";
+
+
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+console.log(currentDate);
 
 $.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    $(".searchBtn").on("click", function() {
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=51eff38dc476b28387cdbdbd9705ea5b&units=imperial";
+    console.log(queryURL);
+    console.log(response);
+    $(".searchBtn").on("click", function () {
+$("div.mainCityName").html("<h3>" + response.name + "</h3>");
+$("div.currentDate").append(currentDate);
+$("div.temp").text(`temperature: ${response.main.temp} degrees F`);
+$("div.humidity").text(`Humidity: ${response.main.humidity}%`);
+$("div.windSpeed").text(`Wind Speed: ${response.wind.speed} mph ${response.wind.deg} degrees`);
 
-    }
-})
+    });
+});
